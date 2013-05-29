68dd41b4a7b45da11391b700c82723b17a247425
sunshine55 <tony.wu90@yahoo.com>
2013-05-29 21:55:05 +0700

    session_spec: change the scenario logout of feature Log out

--- spec/integration/session_spec.rb
+++ spec/integration/session_spec.rb
@@ -1,5 +1,33 @@
 require "spec_helper"
 
+feature "Log in" do
+
+  scenario "Login with valid credentials" do
+    log_in
+  end
+
+  scenario "Login with invalid credentials" do
+    log_in_with :username => 'invaliduser', :password => 'invalidpassword'
+    page.should have_content 'Authentication failed, please check your Username and Password'
+  end
+
+  scenario "Login with no username or password" do
+    log_in_with :username => '', :password => ''
+    page.should have_content 'Authentication failed, please check your Username and Password'
+  end
+
+  scenario "Login with no username" do
+    log_in_with :username => '', :password => 'somepassword'
+    page.should have_content 'Authentication failed, please check your Username and Password'
+  end
+
+  scenario "Login with no password" do
+    log_in_with :username => 'invaliduser', :password => ''
+    page.should have_content 'Authentication failed, please check your Username and Password'
+  end
+end
+
+
 feature "Log out" do
   
   scenario "Verify if users can go to page after logout" do
